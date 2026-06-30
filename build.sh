290617aa027cf40ca4bf9c5a95ff9494168eb443
MAROUANE DARDOUR <marouanedardour2@email.com>
2026-06-30 21:44:43 +0100

    Update gitignore and configuration for production

--- build.sh
+++ build.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 set -o errexit
+echo "Checking if db.sqlite3 exists..."
+if [ -f "db.sqlite3" ]; then
+    echo "db.sqlite3 found."
+else
+    echo "db.sqlite3 not found! Creating it..."
+    python manage.py migrate
+fi
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
