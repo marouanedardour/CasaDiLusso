3252ae6b3b8e00718a44caa22a90a5329ee716f9
MAROUANE DARDOUR <marouanedardour2@email.com>
2026-06-30 21:12:54 +0100

    Update gitignore and configuration for production

--- core/settings.py
+++ core/settings.py
@@ -89,7 +89,7 @@ import dj_database_url
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
