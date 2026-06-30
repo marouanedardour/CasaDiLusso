dada77ceaf2164a1cc64d558d17298b3caf8e985
MAROUANE DARDOUR <marouanedardour2@email.com>
2026-06-30 16:33:34 +0100

    Update gitignore and configuration for production

--- core/settings.py
+++ core/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['CasaDiLusso.onrender.com']
+ALLOWED_HOSTS = ['casadilusso.onrender.com', 'localhost', '127.0.0.1']
 
 # Application definition
 
